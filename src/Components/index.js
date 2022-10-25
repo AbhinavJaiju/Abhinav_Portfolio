b8d5600a17e9c2421cb632df03ccd5906081a421
Abhinav-jaiju <abhinav.jaiju@experionglobal.com>
2022-10-25 18:56:45 +0530

    Commiting before making the final edits

---
+++ src/Components/index.js
@@ -0,0 +1,3 @@
+export { default as SocialMedia} from './SocialMedia/socialMedia';
+export { default as NavigationDots} from './NavigationDots/navigationDots';
+export { default as Navbar } from './Navbar/navbar' 
